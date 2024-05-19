617ce10f00adee3b57335c7c0137375b5b5fb7a4
Sumeet Kumar <122050491+isumeetkumar@users.noreply.github.com>
2024-05-19 17:03:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
         <CurrentLocation />
       </div>
       <div className="footer-info">
-        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
-          Download Source Code
+        <a href="https://www.linkedin.com/in/isumeetkumar/">
+          Contact Me
         </a>{" "}
         | Developed by{" "}
         <a target="_blank" href="https://www.instagram.com/sumeetsinghh_/">
