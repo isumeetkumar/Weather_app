33da04e500b938e3c3e5f614464127bac51c7ea5
Sumeet Kumar <sk483693@gmail.com>
2024-05-19 16:36:50 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "f69410cf3b66f149e24faba878dd0691",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
